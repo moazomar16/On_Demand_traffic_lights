2cc5e2da4d5334f365a79d803f773c672299f61a
Moaz Omar <moazomar16@gmail.com>
2022-09-18 11:29:23 +0200

    Button interfacing using DIO driver

--- ECUAL/BUTTON/BUTTON.c
+++ ECUAL/BUTTON/BUTTON.c
@@ -4,3 +4,15 @@
  * Created: 9/14/2022 8:10:01 PM
  *  Author: moazo
  */ 
+
+#include "BUTTON.h"
+
+// this function is going to initialize the pins to be used with buttons
+void Button_init(uint8_t ButtonPin_Number, uint8_t ButtonPort_Number){
+	DIO_init(ButtonPin_Number, ButtonPort_Number, IN);
+}
+
+// this function is going to check is the button is pressed or not
+void Button_read(uint8_t ButtonPin_Number, uint8_t ButtonPort_Number, uint8_t *value){
+	DIO_read(ButtonPin_Number,ButtonPort_Number,value);
+}
--- ECUAL/BUTTON/BUTTON.h
+++ ECUAL/BUTTON/BUTTON.h
@@ -2,13 +2,21 @@
  * BUTTON.h
  *
  * Created: 9/14/2022 8:10:15 PM
- *  Author: moazo
+ *  Author: Moaz Omar
  */ 
 
 
 #ifndef BUTTON_H_
 #define BUTTON_H_
 
+// including DIO driver 
+#include "../../MCAL/DIO_Driver/DIO.h"
+
+
+
+// APIs
+void Button_init(uint8_t ButtonPin_Number, uint8_t ButtonPort_Number);
+void Button_read(uint8_t ButtonPin_Number, uint8_t ButtonPort_Number, uint8_t *value);
 
 
 
