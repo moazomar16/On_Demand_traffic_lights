2937f76b13667f21b67baf820ffbd5046dde40f0
Moaz Omar <moazomar16@gmail.com>
2022-09-18 11:28:46 +0200

    LED interfacing using DIO driver

--- ECUAL/LED/LED.c
+++ ECUAL/LED/LED.c
@@ -4,3 +4,22 @@
  * Created: 9/14/2022 8:09:08 PM
  *  Author: moazo
  */ 
+
+
+#include "LED.h"
+
+void LED_init(uint8_t LedPin_Number, uint8_t LedPort_Number){
+	DIO_init(LedPin_Number,LedPort_Number,OUT);
+}
+void LED_on(uint8_t LedPin_Number, uint8_t LedPort_Number){
+	DIO_write(LedPin_Number,LedPort_Number,HIGH);
+}
+void LED_off(uint8_t LedPin_Number, uint8_t LedPort_Number){
+	DIO_write(LedPin_Number,LedPort_Number,LOW);
+}
+void LED_toggle(uint8_t LedPin_Number, uint8_t LedPort_Number){
+	DIO_toggle(LedPin_Number,LedPort_Number);
+}
+void LED_blink(uint8_t LedPin_Number, uint8_t LedPort_Number){
+	
+}
--- ECUAL/LED/LED.h
+++ ECUAL/LED/LED.h
@@ -9,6 +9,14 @@
 #ifndef LED_H_
 #define LED_H_
 
+#include "../../MCAL/DIO_Driver/DIO.h"
+
+// APIs
+void LED_init(uint8_t LedPin_Number, uint8_t LedPort_Number);
+void LED_on(uint8_t LedPin_Number, uint8_t LedPort_Number);
+void LED_off(uint8_t LedPin_Number, uint8_t LedPort_Number);
+void LED_toggle(uint8_t LedPin_Number, uint8_t LedPort_Number);
+void LED_blink(uint8_t LedPin_Number, uint8_t LedPort_Number);
 
 
 
