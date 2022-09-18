08f4facd4034234c89fd74db3cda92f0583689d6
Moaz Omar <moazomar16@gmail.com>
2022-09-18 11:23:46 +0200

    typedefs

---
+++ types.h
@@ -0,0 +1,18 @@
+/*
+ * types.h
+ *
+ * Created: 9/16/2022 2:35:18 PM
+ *  Author: moazo
+ */ 
+
+
+#ifndef TYPES_H_
+#define TYPES_H_
+
+
+typedef unsigned char uint8_t;
+typedef volatile unsigned char vuint8_t;
+typedef unsigned int uint16_t;
+
+
+#endif /* TYPES_H_ */
